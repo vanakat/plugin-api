948a412368bf6c2812ffdb6f09a56fdd1d335ed5
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2022-06-06 00:31:44 +0100

    Add initial version of the plugin API helper

---
+++ index.ts
@@ -0,0 +1,17 @@
+import {Plugin} from 'obsidian';
+
+declare global {
+    interface Window {
+        ['PluginApi']?: any
+    }
+}
+
+export function registerAPI(name: string, api: any, plugin: Plugin) {
+    window['PluginApi'] = window['PluginApi'] || {};
+
+    window['PluginApi'][name] = api;
+
+    plugin.register(() => {
+        delete window['PluginApi'][name];
+    });
+}
